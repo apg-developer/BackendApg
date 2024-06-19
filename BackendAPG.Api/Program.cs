e090c3f13871abc15a488428952633140c091cf0
andres perez garrido <apgdeveloper@outlook.com>
2024-06-19 12:44:35 -0500

    Adding CORS policy

--- BackendAPG.Api/Program.cs
+++ BackendAPG.Api/Program.cs
@@ -8,11 +8,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<ISqlMockRepository, SqlMockRepository>();
 builder.Services.AddScoped<IEmployeeBusiness, EmployeeBusiness>();
 
+//CORS enabled for development purposes
+
+builder.Services.AddCors(opt => {
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 
